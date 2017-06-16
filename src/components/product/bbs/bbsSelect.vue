1253c3e0a30bc3a8ba304ce0a9d8771a638dcb03
pengjin0 <827879040@qq.com>
2017-06-16 08:52:18 +0800

    框架初始化

---
+++ src/components/product/bbs/bbsSelect.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="bbs-select">
+		<mt-header title="选择产品">
+		  <router-link to="/" slot="left">
+		    <mt-button icon="back">返回</mt-button>
+		  </router-link>
+		</mt-header>		
+		<div class="bbsImg">
+			<img src="../../../assets/img/bbs.png"/>
+		</div>
+		
+		<dl class="slect_dl">
+			<dt>
+				颜色分类
+			</dt>
+			<dd>
+				<div class="dd_slect dd_active"></div>
+				<div class="dd_slect"></div>
+				<div class="dd_slect"></div>
+				<div class="dd_slect"></div>
+			</dd>
+		</dl>
+		<dl>
+			<dt>选择页数</dt>
+			<dd></dd>
+		</dl>
+		<dl>
+			<dt>选择尺寸</dt>
+			<dd></dd>
+		</dl>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/router.config.js
@@ -0,0 +1,25 @@
+//import Home from './components/Home.vue'
+import Login from './components/Login.vue'
+import User from './components/user/userInfo.vue'
+import Cart from './components/cart/cart.vue'
+//宝宝书选择
+import bbsSelect from './components/product/bbs/bbsSelect.vue'
+
+
+////导出路由
+export default{
+	routes:[
+	 	{ 
+	 		path: '/', component: Login	 		
+	 	},
+		{
+			path:'/user', component:User //用户信息
+		},
+		{
+			path:'/cart', component:Cart //购物车
+		},
+		{
+			path:'/bbsSelect', component:bbsSelect //宝宝书选择
+		}
+	]
+}
